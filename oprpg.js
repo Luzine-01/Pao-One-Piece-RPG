c4827b4393b4d8ccb806534da02c82f69d4cc099
Luzine-01 <luquinhas200749@gmail.com>
2026-07-14 22:12:35 -0300

    msamasmams

--- oprpg.js
+++ oprpg.js
@@ -2,18 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebas
 import { getFirestore, doc, collection, getDoc, getDocs, onSnapshot, setDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 const FB_CONFIG = {
-  apiKey: "AIzaSyBJF3ic2GM_9HADwQGpvX_m__lqw31SzZE",
-  authDomain: "oprpg-bf152.firebaseapp.com",
-  projectId: "oprpg-bf152",
-  storageBucket: "oprpg-bf152.firebasestorage.app",
-  messagingSenderId: "286251090743",
-  appId: "1:286251090743:web:a8366f7c058d4c4a2790b2"
+  apiKey:"AIzaSyCfEtfiU5swXvVkqt4shp8i6h4JYI8ES7U",authDomain:"dand-3c76a.firebaseapp.com",
+  projectId:"dand-3c76a",storageBucket:"dand-3c76a.firebasestorage.app",
+  messagingSenderId:"27455098509",appId:"1:27455098509:web:432929f697da9a947d5cc4",measurementId:"G-D1TQM5WJT8"
 };
-
-// Initialize Firebase
-const fbapp = initializeApp(FB_CONFIG, 'One Piece RPG Pao');
-const db = getFirestore(app);
-const DOC = 'one-piece-campaign'
+const fbApp = initializeApp(FB_CONFIG, 'rwby');
+const db    = getFirestore(fbApp);
+const DOC   = 'rwby-campaign';
 
 // CONSTANTS
 
